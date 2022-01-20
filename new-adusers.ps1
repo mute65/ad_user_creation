9a35c3ea4c772a5ec7810ffededca1d0e53b379a
mute65 <96795789+mute65@users.noreply.github.com>
2022-01-19 20:11:32 -0700

    fish

--- new-adusers.ps1
+++ new-adusers.ps1
@@ -1,4 +1,6 @@
-
+<# Import CSV of users to be created in Active Directory
+CSV Headers as follows: givenName,surname,name,displayName,samAccountName,userPrincipleName,emailAddress,accountPassword,company,targetOU
+Any additional properties can be added as a header and inserted into the New-ADUser cmdlet. #>
 
 
 Import-Module ActiveDirectory
@@ -11,7 +13,7 @@ Import-Module ActiveDirectory
         
         New-ADUser -Server $Server -GivenName $_.givenName -Surname $_.surname -Name $_.name -DisplayName $_.displayName -SamAccountName `
         $_.samAccountName -UserPrincipalName $_.userPrincipleName -EmailAddress $_.emailAddress -Enabled $true -AccountPassword $setpass `
-        -Company $_.company -Path targetOU
+        -Company $_.company -Path $_.targetOU
         
         }
 
