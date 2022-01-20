490feb92027a9e71e32603f92b83380ac87580ad
mute65 <96795789+mute65@users.noreply.github.com>
2022-01-19 20:11:14 -0700

    fish

---
+++ new-adorgunit.ps1
@@ -0,0 +1,20 @@
+<# Import CSV to create Active Directory Organizational Units
+   Main thing to note is to have higher level OUs listed above any 
+   nested OUs in the CSV or you will receive errors that the target 
+   doesn't exist
+   
+   CSV Format as follows: path,name
+
+#>
+
+
+Import-Module ActiveDirectory
+
+    $Server = DOMAINCONTROLLER.FQDN
+
+Import-Csv “c:\path-to-csv.csv” | ForEach-Object {
+
+    New-ADOrganizationalUnit -Server $Server -Path $_.Path -Name $_.Name
+
+}
+
