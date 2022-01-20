896db9ca1ee39f864c483cf75541f7000e757b9b
mute65 <96795789+mute65@users.noreply.github.com>
2022-01-19 17:09:57 -0700

    added script

--- ad_user_creation.ps1
+++ ad_user_creation.ps1
@@ -1,1 +1,18 @@
 
+
+
+Import-Module ActiveDirectory
+
+    $Server = DOMAINCONTROLLER.FQDN
+    
+    Import-Csv c:\path-to-csv.csv | foreach-object {
+        
+        $setpass = ConvertTo-SecureString -AsPlainText $_.accountPassword -force
+        
+        New-ADUser -Server $Server -GivenName $_.givenName -Surname $_.surname -Name $_.name -DisplayName $_.displayName -SamAccountName `
+        $_.samAccountName -UserPrincipalName $_.userPrincipleName -EmailAddress $_.emailAddress -Enabled $true -AccountPassword $setpass `
+        -Company $_.company -Path targetOU
+        
+        }
+
+
